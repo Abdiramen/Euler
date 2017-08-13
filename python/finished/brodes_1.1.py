7978d5ada58438d4d52e2b947b9a87c86c4e4379
a.osman@mst.edu <vimto@mst.edu>
2017-08-13 12:34:38 -0500

    Changed origin

--- python/finished/brodes_1.1.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python3
-print(sum(filter(lambda x: x % 5 == 0 or x % 3 == 0, range(1000))))
--- python/finished/problem6.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python3
-print((lambda n: ( 3 * n ** 4 + 2 * n ** 3 - 3 * n ** 2 - 2 * n )  // 12 )( int( input( "give me n: " ) ) ) )
